f44eaa2d6b910b26efed410d84e5f1de71aac96b
Jai Mehta <54937640+onso19@users.noreply.github.com>
2022-03-06 21:48:41 -0800

    Create P3.cpp

---
+++ DYNAMIC PROGRAMMING/P3.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+//MAXimum sum would be the sum of the array
+int main() {
+	// your code goes here
+    vector<int> v;
+
+    int mccs=0;
+    for(int i=0;i<v.size;i++)
+{int t;
+cin>>t;
+v.push_back(t);
+    mccs+=v[i];
+}
+    return 0;
+}
+
