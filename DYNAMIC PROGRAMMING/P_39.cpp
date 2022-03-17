a02e9656ff79ec30e9a4ff0ff3330ca3da0f32b6
Jai Mehta <54937640+onso19@users.noreply.github.com>
2022-03-18 00:39:44 +0530

    Update P_39.cpp

--- DYNAMIC PROGRAMMING/P_39.cpp
+++ DYNAMIC PROGRAMMING/P_39.cpp
@@ -1,30 +1,24 @@
-/*
-In this solution, we can think about whether the number is even or odd.
-If the number is even, the number of 1s is equal to the number which is half of it.
-For e.g., the number 8 has the same 1s as the number 4.
-The number 10 has the same amount of 1 bits as number 5.
-That is because number i is just left shift by 1 bit from number i / 2.
-Therefore, they should have the same number of 1 bits.
-
-If the numbers are odd, e.g. 1, 3, 5, 7.
-The number of 1 bits is equal to the number (i - 1) plus 1.
-For e.g., for number 3, the number of 1 bits equals to the number 2 plus 1.
-For number 11, it equals to number 10 + 1.
-*/
 class Solution {
 public:
-    vector<int> countBits(int num) {
-        vector<int> res(num);
-        res.push_back(0);  // for num=0
-        if(num==0) return res;
+    int numDecodings(string s) {
+        vector<int> dp(s.size()+1,0);
+        int ans=0;
+        ans = recur(s,0,dp);
+        return ans;
+    }
+    int recur(string s, int pos, vector<int>& dp){
+        if(pos==s.size())return 1;
+        if(s[pos]=='0')return 0;
+        if(pos==s.size()-1)return 1;
+        if(dp[pos]>0)return dp[pos];
+        string b = s.substr(pos,2);
         
-        for(int i=1;i<=num;i++){
-            if(i%2==0){
-                res[i]=res[i/2];
-            } else {
-                res[i]=res[i-1]+1;
-            }
+        int way1 = recur(s,pos+1,dp);
+        int way2=0;
+        if(stoi(b)<=26&&stoi(b)>0){
+            way2 = recur(s,pos+2,dp);
         }
-        return res;
+        dp[pos]= way1+way2;
+        return dp[pos];
     }
 };
